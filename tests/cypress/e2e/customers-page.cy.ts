90c4dc01a66e11f8ecf51e2f221a4d772aa82436
mf0x <mikefox9000@gmail.com>
2025-02-09 07:34:45 -0500

    working test

--- tests/cypress/e2e/customers-page.cy.ts
+++ tests/cypress/e2e/customers-page.cy.ts
@@ -1,14 +1,19 @@
 describe('customers page', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.viewport(1920, 1080)
     cy.visit('/')
-    cy.get("button").click()
-    cy.get(".modal-body").should("exist")
-    cy.getByData("first-name").type("Helen")
-    cy.getByData("last-name").type("Ready")
-    cy.getByData("save-button").click()
-    cy.get(".modal-body").should("not.exist")
-    cy.get(".table-row").contains("Helen")
-    .parent().contains("Ready")
+  })
+
+  context("adding a customer", () => {
+    it('passes', () => {
+      cy.get("button").click()
+      cy.get(".modal-body").should("exist")
+      cy.getByData("first-name").type("Helen")
+      cy.getByData("last-name").type("Ready")
+      cy.getByData("save-button").click()
+      cy.get(".modal-body").should("not.exist")
+      cy.get(".table-row").contains("Helen")
+      cy.get(".table-row").contains("Ready")
     })
+  })  
 })
