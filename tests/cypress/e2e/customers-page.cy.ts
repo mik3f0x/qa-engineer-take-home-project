e84582db41fa0432647e9455142d51d91c51a756
mf0x <mikefox9000@gmail.com>
2025-02-09 18:00:10 -0500

    finish UI tests

--- tests/cypress/e2e/customers-page.cy.ts
+++ tests/cypress/e2e/customers-page.cy.ts
@@ -1,6 +1,6 @@
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
-describe('customers page', () => {
+describe("customers page", () => {
   const testUser = {
     firstName: "",
     lastName: "",
@@ -30,7 +30,7 @@ describe('customers page', () => {
       testUser.zip = faker.location.zipCode('#####')
       testUser.notes = faker.lorem.words(3)
   
-      cy.get("button").click()
+      cy.contains("button", "Add Customer").click() // This button's data-testid="add-customer-button" is not being passed to the DOM, for some reason
       cy.get(".modal-body").should("exist")
       cy.getByData("first-name").type(testUser.firstName)
       cy.getByData("last-name").type(testUser.lastName)
@@ -43,95 +43,95 @@ describe('customers page', () => {
       cy.getByData("notes").type(testUser.notes)
     })
   
-    it('adds a customer with all fields filled and valid', () => {
+    it("adds a customer with all fields filled and valid", () => {
       cy.getByData("save-button").click()
       cy.get(".modal-body").should("not.exist")
 
-      cy.get(".table-row").contains(testUser.firstName)
-      cy.get(".table-row").contains(testUser.lastName)
-      cy.get(".table-row").contains(testUser.email)
-      cy.get(".table-row").contains(testUser.address1)
-      cy.get(".table-row").contains(testUser.address2)
-      cy.get(".table-row").contains(testUser.city)
-      cy.get(".table-row").contains(testUser.state)
-      cy.get(".table-row").contains(testUser.zip)
-      cy.get(".table-row").contains(testUser.notes)
+      cy.get(".table-row").eq(-1).contains(testUser.firstName)
+      cy.get(".table-row").eq(-1).contains(testUser.lastName)
+      cy.get(".table-row").eq(-1).contains(testUser.email)
+      cy.get(".table-row").eq(-1).contains(testUser.address1)
+      cy.get(".table-row").eq(-1).contains(testUser.address2)
+      cy.get(".table-row").eq(-1).contains(testUser.city)
+      cy.get(".table-row").eq(-1).contains(testUser.state)
+      cy.get(".table-row").eq(-1).contains(testUser.zip)
+      cy.get(".table-row").eq(-1).contains(testUser.notes)
     })
 
-    it('adds a customer with only required fields filled and valid', () => {
+    it("adds a customer with only required fields filled and valid", () => {
       cy.getByData("address-line-2").clear()
       cy.getByData("notes").clear()
 
       cy.getByData("save-button").click()
       cy.get(".modal-body").should("not.exist")
 
-      cy.get(".table-row").contains(testUser.firstName)
-      cy.get(".table-row").contains(testUser.lastName)
-      cy.get(".table-row").contains(testUser.email)
-      cy.get(".table-row").contains(testUser.address1)
-      cy.get(".table-row").contains(testUser.city)
-      cy.get(".table-row").contains(testUser.state)
-      cy.get(".table-row").contains(testUser.zip)
+      cy.get(".table-row").eq(-1).contains(testUser.firstName)
+      cy.get(".table-row").eq(-1).contains(testUser.lastName)
+      cy.get(".table-row").eq(-1).contains(testUser.email)
+      cy.get(".table-row").eq(-1).contains(testUser.address1)
+      cy.get(".table-row").eq(-1).contains(testUser.city)
+      cy.get(".table-row").eq(-1).contains(testUser.state)
+      cy.get(".table-row").eq(-1).contains(testUser.zip)
     })
 
-    it('prevents adding a customer with first name blank', () => {
+    it("prevents adding a customer with first name blank", () => {
       cy.getByData("first-name").clear()
 
       cy.getByData("save-button").click()
 
-      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid='error-message'
+      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid="error-message"
       cy.getByData("error-message").contains("You must enter a first name")
-      cy.get('.close-button').click()
+      cy.get(".close-button").click()
       cy.get(".modal-body").should("not.exist")
-      cy.get(".table-row").contains(testUser.email).should('not.exist') // Assuming email is the only unique identifier
+      cy.get(".table-row").eq(-1).contains(testUser.email).should("not.exist") // Assuming email is the only unique identifier
     })
 
-    it('prevents adding a customer with invalid email missing @', () => {
+    it("prevents adding a customer with invalid email missing @", () => {
       cy.getByData("email").clear().type("tomexample.com")
 
       cy.getByData("save-button").click()
 
-      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid='error-message'
+      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid="error-message"
       cy.getByData("error-message").contains("You must enter a valid email address")
-      cy.get('.close-button').click()
+      cy.get(".close-button").click()
       cy.get(".modal-body").should("not.exist")
-      cy.get(".table-row").contains("tomexample.com").should('not.exist') // Assuming email is the only unique identifier
+      cy.get(".table-row").eq(-1).contains("tomexample.com").should("not.exist") // Assuming email is the only unique identifier
     })
 
-    it('prevents adding a customer with invalid email missing dot domain', () => {
+    it("prevents adding a customer with invalid email missing dot domain", () => {
       cy.getByData("email").clear().type("tom@examplecom")
 
       cy.getByData("save-button").click()
 
-      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid='error-message'
+      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid="error-message"
       cy.getByData("error-message").contains("You must enter a valid email address")
-      cy.get('.close-button').click()
+      cy.get(".close-button").click()
       cy.get(".modal-body").should("not.exist")
-      cy.get(".table-row").contains("tom@examplecom").should('not.exist') // Assuming email is the only unique identifier
+      cy.get(".table-row").eq(-1).contains("tom@examplecom").should("not.exist") // Assuming email is the only unique identifier
     })
 
-    it('prevents adding a customer with invalid zip code with 4 digits', () => {
+    it("prevents adding a customer with invalid zip code with 4 digits", () => {
       cy.getByData("zip").clear().type("1111")
 
       cy.getByData("save-button").click()
 
-      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid='error-message'
+      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid="error-message"
       cy.getByData("error-message").contains("You must enter a valid zip code")
-      cy.get('.close-button').click()
+      cy.get(".close-button").click()
       cy.get(".modal-body").should("not.exist")
-      cy.get(".table-row").contains(testUser.email).should('not.exist') // Assuming email is the only unique identifier
+      cy.get(".table-row").eq(-1).contains(testUser.email).should("not.exist") // Assuming email is the only unique identifier
     })
 
-    it('prevents adding a customer with invalid zip code with non-numeric character', () => {
+    it("prevents adding a customer with invalid zip code with non-numeric character", () => {
       cy.getByData("zip").clear().type("11a11")
 
       cy.getByData("save-button").click()
 
-      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid='error-message'
+      // Assuming Save button action is blocked and an error message appears inside the modal with data-testid="error-message"
       cy.getByData("error-message").contains("You must enter a valid zip code")
-      cy.get('.close-button').click()
+      cy.get(".close-button").click()
       cy.get(".modal-body").should("not.exist")
-      cy.get(".table-row").contains(testUser.email).should('not.exist') // Assuming email is the only unique identifier
+      cy.get(".table-row").eq(-1).contains(testUser.email).should("not.exist") // Assuming email is the only unique identifier
     })
 
     // it("prevents adding a customer with all fields blank", () => {})
@@ -139,5 +139,96 @@ describe('customers page', () => {
     // it("prevents adding a customer with injected code in a field", () => {})
     // it("prevents adding a customer with single char entries / consecutive spaces", () => {})
     // Stronger state, zip Code, email validation, etc.
-  })  
+  })
+  
+  
+  context("finding customers", () => {
+    // all tests assume a search field with action on "enter"
+    // Assuming pre-seeded db or production db with test users that can be reset after each test
+    it("finds and displays multiple customers from a text string search", () => {
+      cy.getByData("search-field").type(`.com{enter}`)
+      cy.get(".table-row").should("have.length.greaterThan", 1)
+      cy.get(".table-row").each(($row) => {
+        cy.wrap($row).should("contain.text", ".com")
+      })      
+    })
+
+    it("finds and displays a single customer from an exact match search", () => {
+      cy.getByData("search-field").type(`Test user 1{enter}`)
+      cy.get(".table-row")
+      .should("have.length", 1)
+      .and("contain.text", "Test user 1")
+    })
+
+    it("displays zero table rows when a query match is not found", () => {
+      cy.getByData("search-field").type(`x1x1x1x{enter}`)
+      cy.get(".table-row").should("not.exist") // fix this, negative assertion likely to cause false positive by passing before rows render
+    }) 
+    
+    // What's expected behavior for empty search? Yields all rows?
+  })
+
+
+  context("editing a customer", () => {
+    // Assumptions:
+    //    Table rows are clickable. Clicking on a row opens an "Edit Customer" modal.
+    //    The modal has fields populated with the existing text.
+    //    The modal has a "Save Changes" button That closes it and updates the customer.
+    //    The model has a "Cancel" button which closes it
+    // Assuming pre-seeded db or production db with test users that can be reset after each test
+
+    it("edits an existing customer", () => {
+      const customerToEdit = "Test user 1"
+      const newFirstName = faker.person.firstName()
+      let oldFirstName: string
+
+      cy.contains(".table-row", customerToEdit)
+        .find("td")
+        .eq(0)
+        .then(($td) => {
+          oldFirstName = $td.text()
+        })
+
+      cy.contains(".table-row", customerToEdit).should("exist").click()
+      cy.getByData("customer-modal-body").should("exist")
+      cy.getByData("edit-first-name").type(newFirstName)
+      cy.getByData("save-changes-button").click()
+      cy.getByData("customer-modal-body").should("not.exist")
+
+      cy.contains(".table-row", customerToEdit)
+        .find("td")
+        .eq(0)
+        .then(($td) => {
+          const finalFirstName = $td.text()
+
+          expect(finalFirstName).to.not.equal(oldFirstName)
+          expect(finalFirstName).to.equal(newFirstName)
+        })
+      
+      // Other tests: Updating with invalid entries, etc. Cancel behavior. Delete non-required fields
+    })
+  })
+
+
+  context("deleting a customer", () => {
+    it("deletes a customer", () => {
+      const customerToDelete = "Test user 1"
+
+      cy.contains(".table-row", customerToDelete).should("exist").click()
+    
+      cy.getByData("customer-modal-body").should("exist")
+      cy.getByData("delete-button").click()
+    
+      cy.getByData("delete-modal-body").should("contain.text", "Are you sure you want to delete this customer?")    
+      cy.getByData("ok-button").click()
+    
+      cy.getByData("delete-modal-body").should("not.exist")
+      cy.getByData("customer-modal-body").should("not.exist")
+    
+      cy.getByData("search-field").type(`${customerToDelete}{enter}`)
+      cy.get(".table-row").should("not.exist") // fix this, negative assertion likely to cause false positive by passing before rows render
+    })
+
+    // Also test cancel functionality
+  })
 })
